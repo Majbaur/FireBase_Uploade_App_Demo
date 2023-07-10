379681b93ae3089e0ff04b99f5688f5a0821dcd5
majbaurkhan <mystudy7980@gmai.com>
2023-07-10 22:41:46 +0600

    some comemnts

--- app/src/main/java/com/example/firebaseuploadexample/MainActivity.java
+++ app/src/main/java/com/example/firebaseuploadexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firebaseuploadexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(this, "final Post", Toast.LENGTH_SHORT).show();
     }
 }
